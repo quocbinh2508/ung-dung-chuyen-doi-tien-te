e97b4dfecec464cd7929bece970897ac9b0cddf7
Quoc Binh Nguyen <qbnguyen250897@gmail.com>
2024-11-19 21:28:39 +0700

    ung-dung-thay-doi-tien-te

---
+++ Ung-dung-chuyen-doi-tien-te/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ung_dung_chuyen_doi_tien_te
+{
+    class Program
+{
+
+        static void Main(string[] args)
+        {
+
+            double rate = 23000;
+
+            Console.WriteLine("Nhap so USD ban muon doi sang VND:");
+            double usd = Convert.ToDouble(Console.ReadLine());
+
+            double vnd = usd * rate;
+
+            Console.WriteLine($"Gia tri {usd} USD la {vnd} VND.");
+        }
+    }
+}
